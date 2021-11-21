ffb285559223f744821836dde0a021262f7a065d
Yura Perov <iury.perov@gmail.com>
2021-11-22 03:40:56 +0500

    Added Progress text

--- Assets/Scripts/Model/Player.cs
+++ Assets/Scripts/Model/Player.cs
@@ -1,25 +1,48 @@
 ﻿using Model.Achievements;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Model
 {
     internal class Player : MonoBehaviour
     {
+        private IReadOnlyList<IBrainAchievement> _achievements;
+
         public string Nickname { get; private set; }
         public string Name { get; private set; }
         public string Group { get; private set; }
         public int Progress { get; private set; }
 
+        public event Action<int> OnProgressUpdated;
+
+
+        private void OnEnable()
+        {
+            if (_achievements is null)
+                return;
+
+            foreach (var achivement in _achievements)
+                achivement.OnTargetAchieved += UpdateProgress;
+        }
+
         private void Start()
         {
-            foreach (var achivement in BrainAchievements.Instance.Achievements)
+            _achievements = BrainAchievements.Instance.Achievements;
+            foreach (var achivement in _achievements)
                 achivement.OnTargetAchieved += UpdateProgress;
+        }
+
+        private void OnDisable()
+        {
+            foreach (var achivement in _achievements)
+                achivement.OnTargetAchieved -= UpdateProgress;
         }
 
         private void UpdateProgress(int value)
         {
             Progress += value;
-            Debug.Log(Progress);
+            OnProgressUpdated?.Invoke(Progress);
         }
     }
 }
---
+++ Assets/Scripts/View/Progress.cs
@@ -0,0 +1,32 @@
+﻿using Model;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace View
+{
+    internal sealed class Progress : MonoBehaviour
+    {
+        [SerializeField] private Text _progress;
+        [SerializeField] private Player _player;
+
+        private void OnEnable()
+        {
+            _player.OnProgressUpdated += UpdateState;
+        }
+
+        private void Start()
+        {
+            _progress.text = "0%";
+        }
+
+        private void OnDisable()
+        {
+            _player.OnProgressUpdated -= UpdateState;
+        }
+
+        private void UpdateState(int progress)
+        {
+            _progress.text = progress.ToString() + "%";
+        }
+    }
+}

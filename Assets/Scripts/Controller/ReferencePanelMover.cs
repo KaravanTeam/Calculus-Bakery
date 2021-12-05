2535db3257a5b1d2b2e5b6a9104be9b1eb4dbc17
Yura Perov <iury.perov@gmail.com>
2021-12-05 22:44:30 +0500

    Added Reference panel

---
+++ Assets/Scripts/Controller/ReferencePanelMover.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Controller
+{
+    [RequireComponent(typeof(Animator))]
+    internal sealed class ReferencePanelMover : MonoBehaviour
+    {
+        [SerializeField] private Button _entryButton;
+        [SerializeField] private Button _exitButton;
+
+        private Animator _panelAnimator;
+
+        private readonly string _entryTrigger = "IsEntry";
+        private readonly string _exitTrigger = "IsExit";
+
+        private void Awake()
+        {
+            _panelAnimator = GetComponent<Animator>();
+        }
+
+        private void OnEnable()
+        {
+            _entryButton.onClick.AddListener(PlayEntryAnimation);
+            _exitButton.onClick.AddListener(PlayExitAnimation);
+        }
+
+        private void OnDisable()
+        {
+            _entryButton.onClick.RemoveListener(PlayEntryAnimation);
+            _exitButton.onClick.RemoveListener(PlayExitAnimation);
+        }
+
+        private void PlayEntryAnimation()
+        {
+            _panelAnimator.SetTrigger(_entryTrigger);
+        }
+
+        private void PlayExitAnimation()
+        {
+            _panelAnimator.SetTrigger(_exitTrigger);
+        }
+    }
+}

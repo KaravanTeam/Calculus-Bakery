a269b6a04c8d81f7dac791d1c0f5c71c3a11d082
Yura Perov <iury.perov@gmail.com>
2021-12-06 22:17:36 +0500

    Added menu back button

---
+++ Assets/Scripts/Controller/MenuBackButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace Controller
+{
+    [RequireComponent(typeof(Button))]
+    internal sealed class MenuBackButton : MonoBehaviour
+    {
+        private Button _button;
+
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+        }
+
+        private void OnEnable()
+        {
+            _button.onClick.AddListener(Exit);
+        }
+
+        private void OnDisable()
+        {
+            _button.onClick.RemoveListener(Exit);
+        }
+
+        private void Exit()
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}

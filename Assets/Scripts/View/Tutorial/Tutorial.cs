8987d0c62cd7022fb97181311816542424189c58
Yura Perov <iury.perov@gmail.com>
2021-12-14 10:20:43 +0500

    Added tutorial panels

---
+++ Assets/Scripts/View/Tutorial/Tutorial.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace View
+{
+    [RequireComponent(typeof(Button))]
+    internal sealed class Tutorial : MonoBehaviour
+    {
+        [SerializeField] private TutorialPanel[] _panels;
+
+        [SerializeField] private Button _startButton;
+        [SerializeField] private Button _skipButton;
+        [SerializeField] private GameObject _tutorialPanel;
+
+        //[Header("Backgrounds")]
+        //[SerializeField] private SpriteRenderer _backgroundScene;
+        //[SerializeField] private Image _backgroundCanvas;
+
+        [SerializeField] private Button[] _mainButtons;
+
+        private void Awake()
+        {
+            //_backgroundScene.enabled = true;
+            _tutorialPanel.SetActive(true);
+        }
+
+        private void OnEnable()
+        {
+            _startButton.onClick.AddListener(() => StartCoroutine(Run()));
+            _skipButton.onClick.AddListener(Skip);
+        }
+
+        private void OnDisable()
+        {
+            _startButton.onClick.RemoveAllListeners();
+            _skipButton.onClick.RemoveAllListeners();
+        }
+
+        private void Skip()
+        {
+            _tutorialPanel.SetActive(false);
+        }
+
+        private IEnumerator Run()
+        {
+            _tutorialPanel.SetActive(false);
+            foreach (var button in _mainButtons)
+                button.interactable = false;
+
+            foreach (var panel in _panels)
+            {
+                panel.gameObject.SetActive(true);
+
+                yield return StartCoroutine(panel.Run());
+
+                panel.gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/View/Tutorial/TutorialPanel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace View
+{
+    internal class TutorialPanel : MonoBehaviour, IPointerClickHandler
+    {
+        [SerializeField] protected Animator _replicaAnimator;
+
+        protected bool _isEnd;
+        protected bool _isLoadedPanel;
+
+        public virtual void OnPointerClick(PointerEventData eventData)
+        {
+            if (_isLoadedPanel)
+                _isEnd = true;
+        }
+
+        public virtual IEnumerator Run()
+        { 
+            var waitTimeEntry = _replicaAnimator.GetCurrentAnimatorStateInfo(0).length;
+            for (var time = 0f; time < waitTimeEntry; time += Time.deltaTime)
+            {
+                yield return null;
+            }
+
+            _isLoadedPanel = true;
+
+            yield return new WaitUntil(() => _isEnd);
+
+            _replicaAnimator.SetTrigger("IsEnd");
+            yield return null;
+
+            var waitTime = _replicaAnimator.GetCurrentAnimatorStateInfo(0).length;
+            for (var time = 0f; time < waitTime; time += Time.deltaTime)
+            {
+                yield return null;
+            }
+        }
+    }
+}

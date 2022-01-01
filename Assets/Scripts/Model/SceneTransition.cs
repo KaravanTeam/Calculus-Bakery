5dcd9544741b03e66c0b539000b7bf0316c70831
Yura Perov <iury.perov@gmail.com>
2022-01-01 15:42:09 +0500

    Scene Transition

---
+++ Assets/Scripts/Model/SceneTransition.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Model
+{
+    [RequireComponent(typeof(Animator))]
+    internal sealed class SceneTransition : MonoBehaviour
+    {
+        private Animator _animator;
+        private bool _shouldLoading;
+
+        private readonly string _openingTrigger = "Opening";
+        private readonly string _closingTrigger = "Closing";
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Start()
+        {
+            _animator.SetTrigger(_openingTrigger);
+        }
+
+        public void SwitchToScene(string name)
+        {
+            _animator.SetTrigger(_closingTrigger);
+
+            StartCoroutine(Load(name));
+        }
+
+        public void OnAnimationOver()
+        {
+            _shouldLoading = true;
+        }
+
+        private IEnumerator Load(string name)
+        {
+            yield return new WaitWhile(() => !_shouldLoading);
+
+            SceneManager.LoadScene(name);
+        }
+    }
+}
---
+++ Assets/Scripts/View/CutoutMaskUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+namespace View
+{
+    internal sealed class CutoutMaskUI : Image
+    {
+        public override Material materialForRendering
+        {
+            get
+            {
+                var material = new Material(base.materialForRendering);
+                material.SetInt("_StencilComp", (int)CompareFunction.NotEqual);
+
+                return material;
+            }
+        }
+    }
+}
